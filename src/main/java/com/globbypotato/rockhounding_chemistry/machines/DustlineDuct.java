aa7bb85abe19fbbafa23f2ffdeec8983fa3d9abd
Jorge Bonafé <jorge.bonafe@useargo.com>
2023-01-18 18:04:58 -0300

    Added ability to block ducts with redstone signal Fixed bug in Power Mixer UI

--- src/main/java/com/globbypotato/rockhounding_chemistry/machines/DustlineDuct.java
+++ src/main/java/com/globbypotato/rockhounding_chemistry/machines/DustlineDuct.java
@@ -45,6 +45,65 @@ public class DustlineDuct extends PipelineBase {
 		return false;
 	}
 
+	public void neighborChanged(IBlockState state, World world, BlockPos pos, Block blockIn, BlockPos fromPos) {
+		if (redstoneIsActivated(world, pos)) {
+			onRedstoneActivated(world, pos);
+		}
+	}
+
+
+	@Override
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+		if (redstoneIsActivated(worldIn, pos)) {
+			onRedstoneActivated(worldIn, pos);
+		}
+	}
+
+	private boolean redstoneIsActivated(World world, BlockPos pos) {
+		if (isBlockPowered(world, pos)) {
+			return true;
+		}
+		return false;
+	}
+
+	protected static final void onRedstoneActivated (World worldIn, BlockPos pos) {
+		worldIn.setBlockState(pos, ModBlocks.DUSTLINE_HALT.getDefaultState(), 2);
+	}
+
+	public boolean isBlockPowered(IBlockAccess world, BlockPos pos)
+	{
+		if (this.getRedstoneWeakPower(world, pos.down(), EnumFacing.DOWN) > 0)
+		{
+			return true;
+		}
+		else if (this.getRedstoneWeakPower(world, pos.up(), EnumFacing.UP) > 0)
+		{
+			return true;
+		}
+		else if (this.getRedstoneWeakPower(world, pos.north(), EnumFacing.NORTH) > 0)
+		{
+			return true;
+		}
+		else if (this.getRedstoneWeakPower(world, pos.south(), EnumFacing.SOUTH) > 0)
+		{
+			return true;
+		}
+		else if (this.getRedstoneWeakPower(world, pos.west(), EnumFacing.WEST) > 0)
+		{
+			return true;
+		}
+		else
+		{
+			return this.getRedstoneWeakPower(world, pos.east(), EnumFacing.EAST) > 0;
+		}
+	}
+
+	public int getRedstoneWeakPower(IBlockAccess world, BlockPos pos, EnumFacing facing)
+	{
+		IBlockState iblockstate = world.getBlockState(pos);
+		return iblockstate.getWeakPower(world, pos, facing);
+	}
+
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
 		if(!worldIn.isRemote){
--- src/main/java/com/globbypotato/rockhounding_chemistry/machines/gui/UIPowderMixerController.java
+++ src/main/java/com/globbypotato/rockhounding_chemistry/machines/gui/UIPowderMixerController.java
@@ -58,7 +58,7 @@ public class UIPowderMixerController extends GuiBase {
 		}
  
 		//ingredients
-		if(this.tile.isValidPreset() && this.tile.isValidRecipe()) {
+		if(this.tile.isValidPreset() && this.tile.isValidRecipe() && tile.hasMaterialCabinet() && tile.hasElementsCabinet()) {
 			for(int i = 0;  i < this.tile.getCurrentRecipe().getElements().size(); i++){
 				if(i < 8) {
 					String recipeIngredient = this.tile.getCurrentRecipe().getElements().get(i);
@@ -113,7 +113,7 @@ public class UIPowderMixerController extends GuiBase {
 		}
 		this.fontRenderer.drawString(recipeLabel, 44, 25, 4210752);
 
-		if(this.tile.isValidPreset() && this.tile.isValidRecipe()) {
+		if(this.tile.isValidPreset() && this.tile.isValidRecipe() && tile.hasMaterialCabinet() && tile.hasElementsCabinet()) {
 			for(int x = 0;  x < this.tile.getCurrentRecipe().getElements().size(); x++){
 				if(x < 8) {
 					String recipeIngredient = this.tile.getCurrentRecipe().getElements().get(x);
@@ -161,7 +161,7 @@ public class UIPowderMixerController extends GuiBase {
         }
 
         //quantities
-		if(this.tile.isValidPreset() && this.tile.isValidRecipe()) {
+		if(this.tile.isValidPreset() && this.tile.isValidRecipe() && tile.hasMaterialCabinet() && tile.hasElementsCabinet()) {
 			for(int x = 0;  x < this.tile.getCurrentRecipe().getElements().size(); x++){
 				if(x < 8) {
 					String recipeIngredient = this.tile.getCurrentRecipe().getElements().get(x);
